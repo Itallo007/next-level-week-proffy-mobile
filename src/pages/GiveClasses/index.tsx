a39fa290262b77853f66641c8834b9cbb04c67a4
itallo007 <italosantos.oficial2000@gmail.com>
2020-08-07 16:07:43 -0300

    Implementa a GiveClasses page
    estrutura e estiliza a página
    configura a navegação entre a Landing page e a GiveClasses page

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { useNavigation } from "@react-navigation/native";;
+import {View, Text, ImageBackground} from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
+import giveClassesBgImage from "../../assets/images/give-classes-background.png";
+
+import styles from "./styles";
+
+function GiveClasses() {
+  const {goBack} = useNavigation();
+
+  function hendleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer serum Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+
+      </ImageBackground>
+
+      <RectButton onPress={hendleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
