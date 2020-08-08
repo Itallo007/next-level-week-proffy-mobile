b951f01f7f5aa78a512fc1ceba498b1e142d0ef0
itallo007 <italosantos.oficial2000@gmail.com>
2020-08-08 15:23:15 -0300

    Implementa contato via whatsapp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image, Text} from "react-native";
+import { View, Image, Text, Linking} from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 
 import heartOutLineIcon from "../../assets/images/icons/heart-outline.png";
@@ -20,6 +20,11 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
@@ -51,7 +56,7 @@ const TeacherItem:React.FC<TeacherItemProps> = ({teacher}) => {
             <Image source={unfavorite}/>
           </RectButton>
 
-          <RectButton style={styles.contactButton}>
+          <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
             <Image source={whatsappIcon}/>
             <Text style={styles.contactButtonText}>Entrar em contato</Text>
           </RectButton>
